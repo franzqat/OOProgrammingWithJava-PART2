746e1f63f35fb7a8086e6788a6fcf0ad3d393f0b
Franz <franz.f1032@gmail.com>
2017-07-03 10:45:02 +0200

    7x06,x07

---
+++ week7-week7_06.PromissoryNote/src/PromissoryNote.java
@@ -0,0 +1,29 @@
+
+import java.util.HashMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Francesco
+ */
+public class PromissoryNote {
+    private HashMap<String, Double> debts;
+    public PromissoryNote(){
+        debts = new HashMap<String, Double>();
+        
+    }
+    
+    public void setLoan(String toWhom, double value){
+        debts.put(toWhom, value);
+    }
+    public double howMuchIsTheDebt(String whose){
+        if (debts.containsKey(whose))
+            return debts.get(whose);
+        return 0;
+    }
+}
---
+++ week7-week7_07.Dictionary/src/Dictionary.java
@@ -0,0 +1,46 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Francesco
+ */
+public class Dictionary {
+    private HashMap<String, String> dictionary;
+    public Dictionary(){
+        dictionary = new HashMap<>();
+    }
+    public String translate(String word){ //returning the translation of its parameter. If the word is unknown, it returns null.
+      if (dictionary.containsKey(word))
+              return dictionary.get(word);
+      return null;
+    }
+    
+    public void add(String word, String translation){ // adding a new translation to the dictionary
+        dictionary.put(word, translation);        
+    }
+     public int amountOfWords(){
+         return dictionary.size();
+     }
+     public ArrayList<String> translationList(){
+       return dictionary.entrySet()
+                 .stream()
+             //    .reduce(s->dictionary.keySet() + " = " + dictionary.get(dictionary.keySet()))
+                 .collect(Collectors.toList());
+         
+      /*   
+         ArrayList<String> t = new ArrayList<String>();
+         for (String s : dictionary.keySet())
+             t.add(s);
+         return t;*/
+                     
+     }
+}
